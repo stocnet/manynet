8c35783e5e2031cefecf4acc04057617f37e4df5
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2023-04-17 15:03:56 +0200

    Added southern_women.Rds to test datasets.

--- tests/testthat/test-manip_as.R
+++ tests/testthat/test-manip_as.R
@@ -33,20 +33,20 @@ test_that("data frame converted to matrix correctly",{
   expect_equal(as_matrix(data2), mat2)
 })
 
+southern_women <- readRDS(testthat::test_path("sheets", "southern_women.Rds"))
 test_that("as_matrix converts correctly",{
   expect_vector(as_matrix(mat1))
-  expect_vector(as_matrix(migraph::ison_southern_women))
-  expect_vector(migraph::mpn_elite_mex %>% as_matrix())
-  expect_vector(migraph::mpn_elite_usa_advice %>% as_matrix())
-  expect_equal(as_matrix(as_network(migraph::ison_algebra)),
-               as_matrix(migraph::ison_algebra))
+  expect_vector(as_matrix(southern_women))
+  expect_vector(southern_women %>% as_matrix())
+  expect_vector(southern_women %>% as_matrix())
+  expect_equal(as_matrix(as_network(southern_women)),
+               as_matrix(southern_women))
 })
 
 test_that("as_igraph converts correctly",{
   expect_s3_class(as_igraph(mat1), "igraph")
-  expect_s3_class(as_igraph(migraph::ison_southern_women), "igraph")
-  expect_s3_class(as_igraph(as_network(migraph::ison_southern_women)), "igraph")
-  expect_s3_class(as_igraph(migraph::mpn_elite_usa_money), "igraph")
+  expect_s3_class(as_igraph(southern_women), "igraph")
+  expect_s3_class(as_igraph(as_network(southern_women)), "igraph")
   expect_error(as_igraph(data3, weight = T))
   expect_equal(migraph::network_nodes(as_igraph(as_network(data2))),
                migraph::network_nodes(as_igraph(data2)))
@@ -56,30 +56,24 @@ test_that("as_igraph converts correctly",{
 
 test_that("as_tidygraph converts correctly",{
   expect_s3_class(as_tidygraph(mat1), "tbl_graph")
-  expect_s3_class(as_tidygraph(migraph::ison_southern_women), "tbl_graph")
+  expect_s3_class(as_tidygraph(southern_women), "tbl_graph")
   expect_s3_class(as_tidygraph(as_network(mat1)), "tbl_graph")
-  expect_s3_class(as_tidygraph(as_network(migraph::ison_southern_women)),
+  expect_s3_class(as_tidygraph(as_network(southern_women)),
                   "tbl_graph")
-  expect_s3_class(as_tidygraph(migraph::mpn_elite_usa_money), "tbl_graph")
 })
 
 test_that("as_network converts correctly",{
   expect_s3_class(as_network(mat1), "network")
-  expect_s3_class(as_network(migraph::ison_southern_women), "network")
-  expect_s3_class(as_network(migraph::mpn_elite_usa_money), "network")
+  expect_s3_class(as_network(southern_women), "network")
   expect_equal(as_network(as_network(data2)), as_network(data2))
-  expect_equal(as_network(as_igraph(migraph::ison_marvel_relationships)),
-               as_network(migraph::ison_marvel_relationships))
+  expect_equal(as_network(as_igraph(southern_women)),
+               as_network(southern_women))
   expect_equal(migraph::network_nodes(as_network(dplyr::as_tibble(data2))),
                migraph::network_nodes(as_network(data2)))
-  expect_equal(is_directed(migraph::ison_southern_women),
-               is_directed(as_network(migraph::ison_southern_women)))
-  expect_equal(is_directed(migraph::mpn_DemSxP),
-               is_directed(as_network(migraph::mpn_DemSxP)))
-  expect_equal(is_directed(migraph::ison_adolescents),
-               is_directed(as_network(migraph::ison_adolescents)))
-  expect_equal(is_directed(migraph::ison_algebra),
-               is_directed(as_network(migraph::ison_algebra)))
+  expect_equal(is_directed(southern_women),
+               is_directed(as_network(southern_women)))
+  expect_equal(is_directed(southern_women),
+               is_directed(as_network(southern_women)))
   # NB: ordering of edges is a little different when converting from network
   # to igraph. Should not matter though.
 })
