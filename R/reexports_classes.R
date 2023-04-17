cc1ec52fdec5f1322e0edf2ef0fd93700994aa98
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2023-04-17 17:21:20 +0200

    Commented out helper functions not being currently used in reexports script

--- R/reexports_classes.R
+++ R/reexports_classes.R
@@ -54,14 +54,14 @@ tidygraph::activate
 #' @export
 tidygraph::bind_edges
 
-expect_nodes <- function() {
-  if (!.graph_context$free() && .graph_context$active() != "nodes") {
-    stop("This call requires nodes to be active", call. = FALSE)
-  }
-}
-
-expect_edges <- function() {
-  if (!.graph_context$free() && .graph_context$active() != "edges") {
-    stop("This call requires edges to be active", call. = FALSE)
-  }
-}
+# expect_nodes <- function() {
+#   if (!.graph_context$free() && .graph_context$active() != "nodes") {
+#     stop("This call requires nodes to be active", call. = FALSE)
+#   }
+# }
+# 
+# expect_edges <- function() {
+#   if (!.graph_context$free() && .graph_context$active() != "edges") {
+#     stop("This call requires edges to be active", call. = FALSE)
+#   }
+# }
