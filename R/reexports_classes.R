0576180775657d5875b3748f10bd86ba4fe8f86e
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2023-04-26 12:06:10 +0200

    First attempt to everwrite "tbl_graph" print functions

--- R/reexports_classes.R
+++ R/reexports_classes.R
@@ -65,3 +65,59 @@ tidygraph::bind_edges
 #     stop("This call requires edges to be active", call. = FALSE)
 #   }
 # }
+
+#' Custom print method for "tbl_graph" class
+#'
+#' @param x An object of class "tbl_graph".
+#' @param ... Other arguments passed to or from other methods.
+#' @export print.tbl_graph
+#' @export
+print.tbl_graph <- function(x, ...) {
+  arg_list <- list(...)
+  arg_list[['useS4']] <- NULL
+  graph_desc <- describe_graph(x)
+  not_active <- if (tidygraph::active(x) == 'nodes') 'edges' else 'nodes'
+  top <- suppressWarnings(do.call(tibble::trunc_mat,
+                                  utils::modifyList(arg_list,
+                                                    list(x = tidygraph::as_tibble(x),
+                                                         n = 6))))
+  top$summary[1] <- paste0(top$summary[1], ' (active)')
+  names(top$summary)[1] <- tools::toTitleCase(paste0(substr(tidygraph::active(x),
+                                                            1, 4), ' data'))
+  bottom <- suppressWarnings(do.call(tibble::trunc_mat,
+                                     utils::modifyList(arg_list,
+                                                       list(x = tidygraph::as_tibble(
+                                                         x, active = not_active),
+                                                         n = 3))))
+  names(bottom$summary)[1] <- tools::toTitleCase(paste0(substr(not_active, 1, 4),
+                                                        ' data'))
+  cat_subtle('# A tbl_graph: ', igraph::gorder(x), ' nodes and ',
+             igraph::gsize(x), ' edges\n', sep = '')
+  cat_subtle('#\n')
+  cat_subtle('# ', graph_desc, '\n', sep = '')
+  cat_subtle('#\n')
+  print(top)
+  cat_subtle('#\n')
+  print(bottom)
+  invisible(x)
+}
+
+describe_graph <- function(x) {
+  if (igraph::gorder(x) == 0) {
+    return('An empty graph')
+  } else if (is_directed(x)) {
+    if (is_twomode(x)) {
+      return('A two mode directed graph') 
+    } else {
+      return('A directed graph')
+    }
+  } else {
+    if (!is_directed(x) & is_twomode(x)) {
+      return('A two mode undirected graph')
+    } else {
+      return('An undirected graph')
+    }
+  }
+}
+
+cat_subtle <- function(...) cat(pillar::style_subtle(paste0(...)))
