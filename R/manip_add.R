3552718b98aab8383f87229a9ed3463252cbb856
hollway <hollway@MA0359.local>
2023-08-02 12:49:28 +0200

    Added delete_nodes()

--- R/manip_add.R
+++ R/manip_add.R
@@ -17,9 +17,9 @@
 #' @name add
 NULL
 
-#' @describeIn add Add additional ties to a network
+#' @describeIn add Add additional nodes to a network
 #' @param nodes The number of nodes to be added.
-#' @importFrom igraph add_edges
+#' @importFrom igraph add_vertices
 #' @export
 add_nodes <- function(.data, nodes, attribute = NULL) UseMethod("add_nodes")
 
@@ -36,6 +36,16 @@ add_nodes.tbl_graph <- function(.data, nodes, attribute = NULL){
 #' @export
 add_nodes.network <- function(.data, nodes, attribute = NULL){
   as_network(add_nodes(as_igraph(.data), nodes, attribute))
+}
+
+#' @describeIn add Delete nodes in a network
+#' @importFrom igraph delete_vertices
+#' @export
+delete_nodes <- function(.data, nodes) UseMethod("delete_nodes")
+
+#' @export
+delete_nodes.igraph <- function(.data, nodes){
+  igraph::delete_vertices(.data, v = nodes)
 }
 
 #' @describeIn add Add additional ties to a network
