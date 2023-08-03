71515a049e248cea7bc4816516f59353cb0f0604
hollway <hollway@MA0359.fritz.box>
2023-08-03 06:39:03 +0200

    Added create_explicit()

--- R/make_create.R
+++ R/make_create.R
@@ -358,6 +358,106 @@ create_core <- function(n, directed = FALSE, membership = NULL) {
   }
 }
 
+#' @describeIn create Creates a network based on explicitly
+#'   named nodes and ties between them.
+#'   Wraps `igraph::graph_from_literal()`,
+#'   but never simplifies the result.
+#' @examples
+#'   create_explicit(A -+ B, B -+ C, A +-+ C, D)
+#' @export
+create_explicit <- function(...){
+  if(is.symbol(as.list(match.call())[-1][[1]])){
+    mf <- reformulate(...)
+    mf[[1]] <- NULL
+  } else mf <- as.list(match.call())[-1]
+  f <- function(x) {
+    if (is.call(x)) {
+      return(list(as.character(x[[1]]), lapply(x[-1], f)))
+    }
+    else return(NULL)
+  }
+  ops <- unlist(lapply(mf, f))
+  if (all(ops %in% c("-", ":"))) {
+    directed <- FALSE
+  }
+  else if (all(ops %in% c("-", "+", ":"))) {
+    directed <- TRUE
+  }
+  else {
+    stop("Invalid operator in formula")
+  }
+  f <- function(x) {
+    if (is.call(x)) {
+      if (length(x) == 3) {
+        return(list(f(x[[2]]), op = as.character(x[[1]]), 
+                    f(x[[3]])))
+      }
+      else {
+        return(list(op = as.character(x[[1]]), f(x[[2]])))
+      }
+    }
+    else {
+      return(c(sym = as.character(x)))
+    }
+  }
+  ret <- lapply(mf, function(x) unlist(f(x)))
+  v <- unique(unlist(lapply(ret, function(x) {
+    x[names(x) == "sym"]
+  })))
+  ret <- lapply(ret, function(x) {
+    res <- list()
+    for (i in seq(along.with = x)) {
+      if (x[i] == ":" && names(x)[i] == "op") {
+      }
+      else if (i > 1 && x[i - 1] == ":" && names(x)[i - 
+                                                    1] == "op") {
+        res[[length(res)]] <- c(res[[length(res)]], unname(x[i]))
+      }
+      else {
+        res <- c(res, x[i])
+      }
+    }
+    res
+  })
+  edges <- numeric()
+  for (i in seq(along.with = ret)) {
+    prev.sym <- character()
+    lhead <- rhead <- character()
+    for (j in seq(along.with = ret[[i]])) {
+      act <- ret[[i]][[j]]
+      if (names(ret[[i]])[j] == "op") {
+        if (length(lhead) == 0) {
+          lhead <- rhead <- act
+        }
+        else {
+          rhead <- act
+        }
+      }
+      else if (names(ret[[i]])[j] == "sym") {
+        for (ps in prev.sym) {
+          for (ps2 in act) {
+            if (lhead == "+") {
+              edges <- c(edges, unname(c(ps2, ps)))
+            }
+            if (!directed || rhead == "+") {
+              edges <- c(edges, unname(c(ps, ps2)))
+            }
+          }
+        }
+        lhead <- rhead <- character()
+        prev.sym <- act
+      }
+    }
+  }
+  ids <- seq(along.with = v)
+  names(ids) <- v
+  res <- igraph::make_graph(unname(ids[edges]), 
+                            n = length(v), directed = directed)
+  res <- igraph::set_vertex_attr(res, "name", value = v)
+  as_tidygraph(res)
+}
+
+
 # #' @rdname create
 # #' @details Creates a nested two-mode network.
 # #' Will construct an affiliation matrix,
