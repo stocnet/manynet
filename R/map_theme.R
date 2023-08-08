baf983ca3af9709761fdf1c078a3b2f6cb1379ae
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2023-08-08 09:43:12 +0200

    Fixed small bugs with `theme_iheid()` function

--- R/map_theme.R
+++ R/map_theme.R
@@ -4,15 +4,14 @@
 #' @param ggraph A ggraph object, e.g. created using `autographr()`
 #' @return Themes the current ggraph to current IHEID guidelines.
 #' @examples
-#'  autographr(ison_konigsberg) %>% theme_iheid()
+#' autographr(ison_konigsberg) %>% theme_iheid()
 NULL
 
 #' @rdname themes
 #' @export
 theme_iheid <- function(ggraph) {
-  
-  gg[[2]][[1]]$geom$default_aes$colour <- iheid_palette("IHEID")["IHEIDBlack"] # Text colour
-  gg[[2]][[2]]$geom$default_aes$edge_colour <- iheid_palette("IHEID")["IHEIDGrey"] # Edge colour
-  gg[[2]][[3]]$geom$default_aes$colour <- iheid_palette("IHEID")["IHEIDRed"] # Vertex colour
-  gg
+  ggraph[[2]][[1]]$geom$default_aes$colour <- iheid_palette("IHEID")["IHEIDBlack"] # Text colour
+  ggraph[[2]][[2]]$geom$default_aes$edge_colour <- iheid_palette("IHEID")["IHEIDGrey"] # Edge colour
+  ggraph[[2]][[3]]$geom$default_aes$colour <- iheid_palette("IHEID")["IHEIDRed"] # Vertex colour
+  ggraph
 }
