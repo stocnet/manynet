147d3b2ca4d67336e5cfcddabaa46aedab0b2d2e
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2023-08-09 11:05:04 +0200

    Re-added a few examples in map_autographr script

--- R/map_autographr.R
+++ R/map_autographr.R
@@ -76,7 +76,7 @@ autographr <- function(.data,
 #' @param netlist A list of migraph-compatible networks.
 #' @source http://blog.schochastics.net/post/animating-network-evolutions-with-gganimate/
 #' @examples
-#'   autographs(to_egos(ison_adolescents))
+#' #autographs(to_egos(ison_adolescents))
 #' @export
 autographs <- function(netlist, ...) {
   thisRequires("patchwork")
@@ -105,6 +105,21 @@ autographs <- function(netlist, ...) {
 #' @importFrom ggraph create_layout
 #' @importFrom dplyr mutate select distinct left_join %>%
 #' @source http://blog.schochastics.net/post/animating-network-evolutions-with-gganimate/
+#' @examples
+#' #ison_adolescents %>%
+#' # mutate_ties(year = sample(1995:1998, 10, replace = TRUE)) %>%
+#' # to_waves(attribute = "year") %>%
+#' # autographd()
+#' #ison_adolescents %>%
+#' # mutate(shape = rep(c("circle", "square"), times = 4),
+#' #        color = rep(c("blue", "red"), times = 4),
+#' #        size = sample(4:16, 8, replace = TRUE)) %>%
+#' # mutate_ties(year = sample(1995:1998, 10, replace = TRUE),
+#' #        e_color = sample(c("yellow", "green"), 10, replace = TRUE)) %>%
+#' # to_waves(attribute = "year") %>%
+#' # autographd(keep_isolates = FALSE, layout = "circle", node_shape = "shape",
+#' #            node_color = "color", node_size =  "size",
+#' #            edge_color = "e_color")
 #' @export
 autographd <- function(tlist, keep_isolates = TRUE, layout = "stress",
                        labels = TRUE, node_color = NULL, node_shape = NULL,
