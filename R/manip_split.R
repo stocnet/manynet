ae02721c6a3afad765360471ef3192fa71a9f301
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2023-11-27 17:52:16 +0100

    Added split method for "diff_model" objects

--- R/manip_split.R
+++ R/manip_split.R
@@ -200,13 +200,28 @@ to_waves.igraph <- function(.data, attribute = "wave", panels = NULL) {
 #' @export
 to_waves.data.frame <- function(.data, attribute = "wave", panels = NULL) {
   wp <- unique(tie_attribute(.data, attribute))
-  if(!is.null(panels))
-    wp <- intersect(panels, wp)
+  if(!is.null(panels)) wp <- intersect(panels, wp)
   if(length(wp)>1){
     out <- lapply(wp, function(l) .data[,attribute == l])
     names(out) <- wp
-  } else if(length(wp)>1){
+  } else if(length(wp)>1) {
     out <- .data[,attribute == wp]
+  }
+  out
+}
+
+#' @export
+to_waves.diff_model <- function(.data, attribute = "t", panels = NULL) {
+  if(!is.null(panels)) .data <- .data[.data[[attribute]] %in% panels,]
+  # todo: add metadata to diff_model objects for names/network properties
+  diffusion <- generate_random(max(.data[[attribute]]))
+  out <- list()
+  for (k in .data[[attribute]]) {
+    out[[k + 1]] <- diffusion %>% add_node_attribute("Infected",
+                                                     c(rep("Infected",
+                                                           .data$I[k + 1]),
+                                                       rep("Susceptible",
+                                                           .data$S[k + 1])))
   }
   out
 }
