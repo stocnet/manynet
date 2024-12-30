b1edd0f5fba1c326c19b387a09ed08049a06766b
James Hollway <james.hollway@graduateinstitute.ch>
2024-12-30 20:38:02 +0100

    Updated testing for diffusion measures and marks, added expect_mark test that

--- tests/testthat/helper-manynet.R
+++ tests/testthat/helper-manynet.R
@@ -4,22 +4,41 @@ collect_functions <- function(pattern, package = "manynet"){
   getNamespaceExports(package)[grepl(pattern, getNamespaceExports(package))]
 }
 
-expect_values <- function(object, ref) {
+expect_values <- function(object, ref, toler = 3) {
   # 1. Capture object and label
   # act <- quasi_label(rlang::enquo(object), arg = "object")
   act <- list(val = object, label = deparse(substitute(object)))
   
   # 2. Call expect()
-  act$n <- unname(unlist(act$val))
-  ref <- unname(unlist(ref))
+  act$n <- round(c(unname(unlist(act$val))), toler)
+  ref <- round(c(unname(unlist(ref))), toler)
   expect(
     act$n == ref,
-    sprintf("%s has values %i, not values %i.", act$lab, act$n, ref)
+    sprintf("%s has values %f, not values %f.", act$lab, act$n, ref)
   )
   
   # 3. Invisibly return the value
   invisible(act$val)
 }
+
+expect_mark <- function(object, ref, top = 3) {
+  # 1. Capture object and label
+  # act <- quasi_label(rlang::enquo(object), arg = "object")
+  act <- list(val = object, label = deparse(substitute(object)))
+  
+  # 2. Call expect()
+  act$n <- as.character(c(unname(unlist(act$val)))[1:top])
+  ref <- as.character(c(unname(unlist(ref)))[1:top])
+  expect(
+    all(act$n == ref),
+    sprintf("%s has values %s, not values %s.", act$lab, 
+            paste(act$n, collapse = ", "), paste(ref, collapse = ", "))
+  )
+  
+  # 3. Invisibly return the value
+  invisible(act$val)
+}
+
 
 top3 <- function(res, dec = 4){
   if(is.numeric(res)){
--- tests/testthat/test-mark_nodes.R
+++ tests/testthat/test-mark_nodes.R
@@ -51,31 +51,20 @@ test_that("node_is_min works", {
 })
 
 test_that("additional node mark functions work", {
+  expect_mark(node_is_independent(ison_adolescents), c(TRUE,FALSE,F))
+  expect_mark(node_is_core(ison_adolescents), c(F,T,T))
+  expect_mark(node_is_mentor(ison_adolescents), c(F,T,T))
+  expect_mark(node_is_latent(play_diffusion(create_tree(6), latency = 1), time = 1),
+               c(F,T,T))
+  expect_mark(node_is_infected(play_diffusion(create_tree(6)), time = 1),
+               c(T,T,T,F,F,F), top = 6)
+  set.seed(123)
+  expect_mark(node_is_recovered(play_diffusion(create_tree(12), recovery = 0.3), time = 3),
+               c(T,T,T,F,F,F), top = 6)
+  expect_mark(node_is_exposed(manynet::create_tree(6), mark = c(1,3)),
+               c(F,T,F))
   set.seed(1234)
-  expect_equal(as.character(node_is_independent(ison_adolescents)),
-               c("TRUE", "FALSE", "FALSE", "FALSE", "TRUE", "TRUE", "FALSE", "TRUE"))
-  set.seed(1234)
-  expect_equal(as.character(node_is_core(ison_brandes)),
-               c("FALSE", "FALSE", "TRUE", "TRUE", "FALSE", "FALSE", "FALSE",
-                 "FALSE", "TRUE", "FALSE", "FALSE"))
-  set.seed(1234)
-  expect_equal(as.character(node_is_mentor(ison_adolescents)),
-               c("FALSE", "TRUE", "TRUE", "FALSE", "FALSE", "FALSE", "FALSE", "FALSE"))
-  set.seed(1234)
-  expect_equal(as.character(node_is_latent(play_diffusion(create_tree(6), latency = 1), time = 1)),
-               c("FALSE", "TRUE",  "TRUE",  "FALSE", "FALSE", "FALSE"))
-  set.seed(1234)
-  expect_equal(as.character(node_is_infected(play_diffusion(create_tree(6)), time = 1)),
-               c("TRUE", "TRUE",  "TRUE",  "FALSE", "FALSE", "FALSE"))
-  set.seed(1234)
-  expect_equal(as.character(node_is_recovered(play_diffusion(create_tree(6), recovery = 0.5), time = 3)),
-               c("TRUE", "TRUE",  "TRUE",  "FALSE", "FALSE", "FALSE"))
-  set.seed(1234)
-  expect_equal(as.character(node_is_exposed(manynet::create_tree(6), mark = c(1,3))),
-               c("FALSE", "TRUE",  "FALSE",  "FALSE", "FALSE", "TRUE"))
-  set.seed(1234)
-  expect_equal(as.character(node_is_random(ison_adolescents, 1)),
-               c("FALSE", "FALSE",  "FALSE",  "TRUE", "FALSE", "FALSE", "FALSE", "FALSE"))
+  expect_mark(node_is_random(ison_adolescents, 2), c(F,T,F))
 })
 
 test_that("node infection, exposure, and recovery works", {
