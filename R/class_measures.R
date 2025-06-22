714ae1ef05e5225078db87c975e9e4e834d26eb9
James Hollway <james.hollway@graduateinstitute.ch>
2025-06-22 18:46:07 +0200

    Dropped old plotting methods, now in autograph

--- R/class_measures.R
+++ R/class_measures.R
@@ -28,16 +28,6 @@ make_network_measure <- function(out, .data, call) {
   out
 }
 
-make_network_measures <- function(out, .data) {
-  time <- value <- NULL
-  out <- dplyr::as_tibble(out) %>% 
-    dplyr::mutate(time = as.numeric(names(out))) %>% 
-    dplyr::select(time, value)
-  class(out) <- c("network_measures", class(out))
-  attr(out, "mode") <- net_dims(.data)
-  out
-}
-
 # Printing ####
 #' @importFrom cli spark_bar
 #' @export
@@ -135,70 +125,3 @@ summary.network_measure <- function(object, ...,
                 ", p = ", cli::style_italic(round(p,3)),
                 ")")
 }
-
-# Plotting ####
-#' @export
-plot.node_measure <- function(x, type = c("h", "d"), ...) {
-  #type <- match.arg(type)
-  density <- NULL
-  if (is.null(attr(x, "mode"))) attr(x, "mode") <- rep(FALSE, length(x))
-  data <- data.frame(Score = x, Mode = attr(x, "mode"))
-  if (length(type) == 2) {
-    p <- ggplot2::ggplot(data = data, ggplot2::aes(x = .data$Score)) +
-      ggplot2::geom_histogram(ggplot2::aes(y = ggplot2::after_stat(density)),
-                              binwidth = ifelse(max(data$Score) > 1, 1,
-                                                ifelse(max(data$Score) > 
-                                                         .1, .1, .01))) +
-      ggplot2::geom_density(col = 2) +
-      ggplot2::scale_y_continuous("Frequency", sec.axis = 
-                                    ggplot2::sec_axis(~ ., breaks = c(0,1),
-                                                      name = "Density"))
-  } else if (length(type) == 1 & type == "h") {
-    p <- ggplot2::ggplot(data = data, ggplot2::aes(x = .data$Score)) +
-      ggplot2::geom_histogram(ggplot2::aes(y = ggplot2::after_stat(density)),
-                              binwidth = ifelse(max(data$Score) > 1, 1,
-                                                ifelse(max(data$Score) >
-                                                         .1, .1, .01))) +
-      ggplot2::labs(x = "Density", y = "Frequency")
-  } else if (length(type) == 1 & type == "d") {
-    p <- ggplot2::ggplot(data = data, ggplot2::aes(x = .data$Score)) +
-      ggplot2::geom_density(col = 2) +
-      ggplot2::ylab("Density")
-  }
-  p +
-    ggplot2::theme_classic() +
-    ggplot2::theme(panel.grid.major = ggplot2::element_line(colour = "grey90"))
-}
-
-#' @export
-plot.tie_measure <- function(x, type = c("h", "d"), ...) {
-  type <- match.arg(type)
-  data <- data.frame(Score = x)
-  if (type == "h") {
-    p <- ggplot2::ggplot(data = data) +
-      ggplot2::geom_histogram(ggplot2::aes(x = .data$Score),
-                              binwidth = ifelse(max(data$Score) > 1, 1,
-                                                ifelse(max(data$Score) > .1,
-                                                       .1,
-                                                       .01))) +
-      ggplot2::ylab("Frequency")
-  } else {
-    p <- ggplot2::ggplot(data = data) +
-      ggplot2::geom_density(ggplot2::aes(x = .data$Score)) +
-      ggplot2::ylab("Density")
-  }
-  p + ggplot2::theme_classic() +
-    ggplot2::theme(panel.grid.major = ggplot2::element_line(colour = "grey90"))
-}
-
-#' @export
-plot.network_measures <- function(x, ...) {
-  ggplot2::ggplot(data = x, ggplot2::aes(x = .data$time, y = .data$value)) +
-    ggplot2::geom_line() +
-    ggplot2::theme_minimal() +
-    ggplot2::xlab("Time") +
-    ggplot2::ylab("Value")
-}
-
-# defining global variables more centrally
-utils::globalVariables(c(".data"))
