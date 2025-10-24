1155b81a65ef47619d7845725898d90dea54f472
James Hollway <james.hollway@graduateinstitute.ch>
2025-10-24 14:21:01 +0200

    Testing member printing

--- tests/testthat/test-member_cliques.R
+++ tests/testthat/test-member_cliques.R
@@ -3,4 +3,6 @@ test_that("node_in_roulette works", {
   expect_s3_class(res, "node_member")
   expect_length(res, net_nodes(ison_adolescents))
   expect_false(res[1] == res[2])
+  expect_output(print(res), "3 groups")
+  expect_output(print(summary(res)), "Class A:")
 })
