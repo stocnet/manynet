517ddfb56173c3f798ef699dc69ea7e7deb30bf7
James Hollway <james.hollway@graduateinstitute.ch>
2025-10-24 14:21:23 +0200

    Testing motif printing

--- tests/testthat/test-motif_census.R
+++ tests/testthat/test-motif_census.R
@@ -2,39 +2,44 @@
 set.seed(123)
 task_eg <- to_named(to_uniplex(ison_algebra, "tasks"))
 
-test <- node_by_tie(task_eg)
-test_that("node tie census works", {
+test_that("node_by_tie census works", {
+  test <- node_by_tie(task_eg)
   expect_equal(test[1:4], rep(0, 4))
   expect_s3_class(test, "node_motif")
+  expect_output(print(test), "fromA")
+  expect_equal(nrow(summary(test, membership = node_in_roulette(task_eg, 3))),
+               3)
 })
 
-test <- node_by_triad(task_eg)
-test_that("node triad census works", {
+test_that("node_by_triad census works", {
+  test <- node_by_triad(task_eg)
   expect_equal(top3(test[,16]), c(7,8,6))
   expect_s3_class(test, "node_motif")
   expect_equal(colnames(test)[1:3], c("003", "012", "102"))
 })
 
-test <- net_by_dyad(ison_adolescents)
-test_that("net_dyad census works", {
+test_that("net_by_dyad census works", {
+  test <- net_by_dyad(ison_adolescents)
   expect_equal(test[[1]], 10)
   expect_equal(test[[2]], 18)
   expect_equal(names(test), c("Mutual", "Null"))
   expect_s3_class(test, "network_motif")
+  expect_output(print(test), "Mutual")
 })
 
-test <- net_by_triad(ison_adolescents)
-test_that("net_triad census works", {
+test_that("net_by_triad census works", {
+  test <- net_by_triad(ison_adolescents)
   expect_equal(test[[1]], 13)
   expect_equal(test[[3]], 29)
   expect_equal(names(test), c("003", "012", "102", "201", "210", "300"))
   expect_s3_class(test, "network_motif")
+  expect_equal(names(summary(test)), c("003", "012", "102", "201", "210", "300"))
   # Error
   expect_error(net_by_triad(ison_southern_women))
 })
 
-test <- node_by_tetrad(ison_southern_women)
-test_that("node tetrad census works", {
+test_that("node_by_tetrad census works", {
+  test <- node_by_tetrad(ison_southern_women)
   expect_s3_class(test, "node_motif")
   expect_equal(test[1,1], 1241)
 })
