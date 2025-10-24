40d2cf2723f27c12f09f69906223549ba3765fe8
James Hollway <james.hollway@graduateinstitute.ch>
2025-10-24 07:25:10 +0200

    Added some feature tests

--- tests/testthat/test-measure_features.R
+++ tests/testthat/test-measure_features.R
@@ -3,6 +3,7 @@ set.seed(123)
 test_that("small-world metrics for two mode networks are calculated and displayed correctly", {
   expect_s3_class(net_smallworld(ison_southern_women), "network_measure")
   expect_equal(as.numeric(net_smallworld(ison_southern_women)), -0.94, tolerance = 0.02)
+  expect_equal(c(net_smallworld(ison_adolescents, method = "SWI")), -0.25, tolerance = 0.05)
 })
 
 # test_that("net_balance works", {
@@ -24,6 +25,16 @@ test_that("net_core works", {
   out <- net_core(ison_adolescents)
   expect_s3_class(out, "network_measure")
   expect_length(out, 1)
+  expect_values(out, -0.133)
+  expect_values(net_core(ison_adolescents, method = "ident"), 6.481)
+  expect_values(net_core(ison_adolescents, method = "diff"), 6.094)
+})
+
+test_that("net_richclub works", {
+  out <- net_richclub(ison_adolescents)
+  expect_s3_class(out, "network_measure")
+  expect_length(out, 1)
+  expect_values(out, 0.833)
 })
 
 test_that("net_factions works", {
