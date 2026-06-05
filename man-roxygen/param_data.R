c8117f5b98c473885df78e926a827a27aaa62659
James Hollway <james.hollway@graduateinstitute.ch>
2026-06-05 20:30:32 +0200

    Updated param_data to include the stocnet object

--- man-roxygen/param_data.R
+++ man-roxygen/param_data.R
@@ -1,8 +1,9 @@
 #' @param .data An object of a `{manynet}`-consistent class:
 #'   \itemize{
-#'   \item matrix (adjacency or incidence) from `{base}` R
-#'   \item edgelist, a data frame from `{base}` R or tibble from `{tibble}`
-#'   \item igraph, from the `{igraph}` package
-#'   \item network, from the `{network}` package
-#'   \item tbl_graph, from the `{tidygraph}` package
+#'   \item adjacency or incidence `matrix` from `{base}` R
+#'   \item edgelist `data.frame` from `{base}` R or `tbl/tbl_df` from `{tibble}`
+#'   \item stocnet `stocnet`, from the `{manynet}` package
+#'   \item igraph `igraph`, from the `{igraph}` package
+#'   \item network `network`, from the `{network}` package
+#'   \item tidygraph `tbl_graph`, from the `{tidygraph}` package
 #'   }
