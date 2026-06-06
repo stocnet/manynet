eb674733e966067322c8a1aa7ba26a0e931f44e7
James Hollway <james.hollway@graduateinstitute.ch>
2026-06-06 10:20:24 +0200

    Added mutate_globals()

---
+++ R/manip_global.R
@@ -0,0 +1,50 @@
+#' Manipulating global attributes
+#' @name manip_global
+#' @description
+#'   These functions offer ways to manipulate network-level data constants
+#'   or variables that are not tied to a particular node or tie.
+#'   They include:
+#'   
+#'   - `mutate_globals()` adds a table of global variables to the network.
+#'   
+#'   It expects three columns for  
+#'   the variable to which the change applies, which should be called 'var', 
+#'   the time of the change, which should be called 'time',
+#'   and the new value to be applied, which should be called 'value'.
+#' @template param_data
+#' @template param_dots
+#' @template param_by
+#' @family global
+#' @eval detail_avail(".*_global")
+#' @template fam_manip
+#' @seealso [to_time()]
+NULL
+
+#' @rdname manip_global
+#' @examples
+#' add_globals(ison_algebra, 
+#'             data.frame(wave = 2, node = 1, var = "active", value = FALSE))
+#' @export
+mutate_globals <- function(.data, ...) UseMethod("mutate_globals")
+
+#' @export
+mutate_globals.default <- function(.data, ...){
+  as_input(.data, mutate_globals, ...)
+}
+
+#' @export
+mutate_globals.tbl_graph <- function(.data, ...){
+  globals <- igraph::graph_attr(.data, "globals")
+  globals <- tidygraph::mutate(globals, ...)
+  igraph::graph_attr(.data, "globals") <- globals
+  .data
+}
+
+#' @export
+mutate_globals.stocnet <- function(.data, ...){
+  out <- .data
+  globals <- out$globals
+  globals <- dplyr::mutate(globals, ...)
+  out$globals <- globals
+  out
+}
